7f23eb77896ea5fdceeae2683b31e0fc1fb9751d
Vighnesh Venkatakrishnan <venkatakrishnan.v@husky.neu.edu>
2018-09-15 22:27:55 -0400

    provision to add empty list and ignore multiple commas

--- ParanthesisTreePrinter.py
+++ ParanthesisTreePrinter.py
@@ -1,35 +1,37 @@
 '''
-A valid input is an input having equal number of opening and closing square brackets where closing brackets always follow the opening ones.
-This python file prints valid inputs as per the following input design :
+	A valid input is an input having equal number of opening and closing square brackets where closing brackets always follow the opening ones.
+	This python file prints valid inputs as per the following input design :
 
-  - Individual elements are delimited with a comma followed by space (e.g. ", ")
-  - Individual groupings are designated with an open bracket [ and a close bracket ]
-  - Empty elements are valid (e.g. [], "").
+  	- Individual elements are delimited with a comma followed by space (e.g. ", ")
+  	- Individual groupings are designated with an open bracket [ and a close bracket ]
+  	- Empty elements are valid (e.g. [], "").
 
-(Input): [1, 2, [A, B, C, [5^&, )()6, , 7], D, E], 3, 4]
-(Output):
-1
-2
-	A
-	B
-	C
-		5^&
-		)()6
+	(Input): [1, 2, [A, B, C, [5^&, )()6, , 7], D, E], 3, 4]
+	(Output):
+	1
+	2
+		A
+		B
+		C
+			5^&
+			)()6
 
-		7
-	D
-	E
-3
-4
+			7
+		D
+		E
+	3
+	4
 
-If the string input is invalid, print "Invalid!" and exit.
-(Input): [1, 2, [A, B, C, [5, 6]]
-(Output): Invalid!
+	If the string input is invalid, print "Invalid!" and exit.
+	(Input): [1, 2, [A, B, C, [5, 6]]
+	(Output): Invalid!
 '''
 
 def paranthesesChecker(tree):
 	'''
-	Check if given tree has equal number of closing and opening square brackets using the paranthesesStack
+		Check if given tree has equal number of closing and opening square brackets using the paranthesesStack
+		input : tree as string
+		output : boolean value indicating paranthesesCheck
 	'''
 	paranthesesStack = []
 
@@ -48,7 +50,14 @@ def paranthesesChecker(tree):
 		return False
 
 def tabs(count):
+	'''
+		Return String with n number of tabs
+		input : number of tabs needed
+		output : string with tabs = count
+	'''
 	emptySpaces = ''
+	if(count <= 0):
+		return emptySpaces
 	while(count > 0):
 		emptySpaces = emptySpaces + '    '
 		count = count - 1
@@ -65,23 +74,38 @@ if(string_input[0] is not '[' or string_input[len(string_input)-1] is not ']'):
 
 #remove outermost square brackets
 #string_input = string_input[1:len(string_input)-1]
+
 #Validate for equal number of square parantheses	
 if(paranthesesChecker(string_input) is False):
 	print("Invalid")
 	exit()
+
+#commas not allowed in elements stripped by commas
+for elem in string_input.split(", "):
+	if(',' in elem):
+		print("Invalid")
+		exit()
 try:
-	bracketCount = 0
-	for elem in string_input.split(", "):
-		if(elem.strip() == '[]' or elem.strip() == ''):
-			print(tabs(bracketCount))
-		elif('[' in elem):
+	bracketCount = 0 														#count of currently open square brackets
+	for elem in string_input.split(", "): 									#split strings at commas
+		elem = elem.strip()
+		if('[' in elem and ']' in elem): 									#if empty list exists in elem
+			if(elem.count('[') ==elem.count(']')): 							#equal square brackets in elem indicates complete empty sublist
+				print(tabs(bracketCount))
+			elif(elem.count('[') > elem.count(']')): 						#if more open brackets, increase bracket count
+				bracketCount = bracketCount + 1
+				print(tabs(bracketCount))
+			else: 															#if more closed brackets, decrease bracket count
+				print(tabs(bracketCount))
+				bracketCount = bracketCount - 1
+		elif('[' in elem): 													#if brackets open, add spaces
 			bracketCount = bracketCount + 1
 			print(tabs(bracketCount - 1) + elem[1:])
-		elif(']' in elem):
+		elif(']' in elem): 													#if brackets close, remove spaces
 			print(tabs(bracketCount - 1) + elem[:len(elem)-1])
 			bracketCount = bracketCount - 1
 		else:
-			print(tabs(bracketCount) + elem)
+			print(tabs(bracketCount - 1) + elem)
 except:
 	print("Invalid!")
 
